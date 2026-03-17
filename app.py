d894e72e6462f63542a39b6783b7217d3b902bcd
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-03-18 11:38:55 +1300

    save

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, g
+import sqlite3
+
+
+DATABASE = 'database.db'
+
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
