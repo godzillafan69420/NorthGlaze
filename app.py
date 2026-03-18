02d3022085c96a37a6d5e69cd09d0833ff61fbb7
Kevin XU <160812274+godzillafan69420@users.noreply.github.com>
2026-03-18 16:35:39 +1300

    W work flow

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g, render_template
 import sqlite3
 
 
@@ -13,6 +13,12 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
@@ -23,8 +29,10 @@ def close_connection(exception):
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    sql = "SELECT house_points.south_point, house_points.north_point, house_points.west_point FROM house_points;"
+    results = query_db(sql)
+    return render_template("layout.html", house_points=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
